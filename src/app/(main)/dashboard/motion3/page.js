a02ec07afea2ebf4d87c26a91dd347627ebaa9a3
Tomas <tomaslonnblad@gmail.com>
2026-02-02 16:24:28 +0100

    Add files via upload

---
+++ src/app/(main)/dashboard/motion3/page.js
@@ -0,0 +1,27 @@
+
+
+ export default function Page() {
+  return (
+    <div className="flex h-full flex-col items-center justify-center space-y-2 text-center">
+      <h1 className="font-semibold text-2xl">3D Design</h1>
+      <p className="text-muted-foreground">
+3D Nodes group
+</p>
+
+<br></br>
+      <h1>Shadcn and radix component with Next.js</h1>
+      <p>
+        This page demonstrates the usage of the 3D nextjs and on html{" "}
+        <a href="https://tomaslonnblad.github.io/">
+          and Gesap with the designm tool tailwind.
+        </a>{" "}
+        Different examples.
+      </p>
+
+    </div>
+  );
+}
+      <div>
+    </div>
+
+
